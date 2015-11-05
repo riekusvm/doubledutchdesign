d89ed023d98caacf88601369f3e42c9918683826
Erik Veld <eveld@xebia.com>
2015-11-05 22:19:33 +0100

    First implementation of querying the data

--- src/containers/application/application.js
+++ src/containers/application/application.js
@@ -2,9 +2,48 @@ import React from 'react';
 import css from './application.css'
 
 export default class Application extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentWillMount = () => {
+    let products = [];
+    $.ajax({
+      url: 'http://double-dutch-design.myshopify.com/products.json',
+        dataType: 'json',
+        cache: false,
+      success: function(data) {
+        products = data.products;
+      }.bind(this),
+        error: function(xhr, status, err) {
+          // console.error('http://double-dutch-design.myshopify.com/products.json', status, err.toString());
+      }.bind(this)
+    }).then(() => {
+      // Format the product
+      this.setState({products: products});
+    });
+  }
+
+  getProducts = () => {
+    console.log(this.state.products);
+    return this.state.products;
+    // let products = [];
+    // this.state.products.map(function(product) {
+    //   products.push(<div>{product}</div>);
+    //   console.log(product);
+    // });
+    // return products;
+  }
+
   render = () => {
+    let products = this.getProducts();
     return (
-      <div className={css.application}>EPIC APPLICATION</div>
+      <div className={css.application}>
+        {products}
+      </div>
     );
   }
 }
